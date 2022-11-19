28fcf76ef734c170d74bfc89c7d94e453e3be6c0
Семен Киреков <sakireko@mts.ru>
2022-11-19 22:06:29 +0300

    add flyway migration

---
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE article
+(
+    id      UUID PRIMARY KEY,
+    name    VARCHAR(200)  NOT NULL,
+    content TEXT          NOT NULL,
+    tags    VARCHAR(20)[] NOT NULL,
+    likes   BIGINT        NOT NULL
+);
+
+CREATE TABLE comment
+(
+    id      UUID PRIMARY KEY,
+    content TEXT
+);
